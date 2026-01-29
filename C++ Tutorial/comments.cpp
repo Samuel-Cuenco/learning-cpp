27a6e2ce0b7ef72d60fe149b6dcb8cde1edb9e59
Samuel <cuencosamuelv@gmail.com>
2026-01-30 01:47:14 +0900

    comments in c++

---
+++ C++ Tutorial/comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    // This is a comment
+    cout << "This is a print statement";
+
+    /* This is a Multi-line comment 
+        You can add as many lines
+        of comments in between
+        these.
+    */
+}
+
+// ln5: This is a comment, very useful for documenting your code
+// ln8: This is a multi-line comment

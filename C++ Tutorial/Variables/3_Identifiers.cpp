48978c9625622749aa0e89d07035e04eba248390
Samuel <cuencosamuelv@gmail.com>
2026-02-06 05:14:08 +0900

    Create 3_Identifiers.cpp

---
+++ C++ Tutorial/Variables/3_Identifiers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Good
+    int minutesPerHour = 60;
+
+    // Fine, but doesn't say what m actually is
+    int m = 60;
+    
+    return 0;
+}
+
+// Variable names should be unique
+// These variable names are called identifiers
+// It is recommended to have descriptive names for variables
+
+// Names can contain letters, digits and underscores.
+// Names must begin with a letter or an underscore
+// Names are case sensitive (number and nUmBeR are different from each other)
+// Names cannot contain whitespaces or special characters
+// Reserved words like c++ keywords such as int, return, char, etc. cannot be used as names

34eedeb8c06538978ac908c5a53c3de8bbe6880b
Samuel <cuencosamuelv@gmail.com>
2026-09-01 05:28:49 +0900

    lts

---
+++ C++ Tutorial/User_Input.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // cout belongs here
+using std::cout;
+using std::cin;
+
+int main (void) {
+    int num; // declare a variable to store your input
+    cout << "Type a number: \n";
+    cin >> num; // takes user input and places it in the num variable
+    cout << "Your number is: " << num; // Display the Value of the variable num
+
+    return 0;
+}
+
+// This is a simple user input
